864c712d60a97d87642c1839f5d94a62c1bb7f28
hugoxxxx <120097527@qq.com>
2019-01-29 22:38:37 +0800

    Initial commit

---
+++ flaskr/__init__.py
@@ -0,0 +1,15 @@
+from flask import Flask
+
+
+def create_app(test_config=None):
+    app = Flask(__name__, instance_relative_config=True)
+
+    @app.route('/hello')
+    def hello():
+        return 'Hello, World!'
+
+    from . import auth
+    app.register_blueprint(auth.bp)
+
+    return app
+
