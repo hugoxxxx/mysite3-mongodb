844d047add1334f67ae00b362d9ba694ee851fa9
hugox <120097527@qq.com>
2019-01-30 15:46:06 +0800

    1

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -3,6 +3,9 @@ from flask import Flask
 
 def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY='dev',
+    )
 
     @app.route('/hello')
     def hello():
--- flaskr/auth.py
+++ flaskr/auth.py
@@ -4,11 +4,19 @@ from flask import (
     Blueprint, flash, g, redirect, render_template, request, session, url_for
 )
 from werkzeug.security import check_password_hash, generate_password_hash
-from flaskr.db import get_db
 from pymongo import MongoClient
 
 
+
 bp = Blueprint('auth', __name__, url_prefix='/auth')
+
+
+def encrypt_passowrd(password):
+    return generate_password_hash(password)
+
+
+def verify_password(user_password, password):
+    return check_password_hash(user_password, password)
 
 
 @bp.route('/register', methods=('GET', 'POST'))
@@ -16,26 +24,55 @@ def register():
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
-        db = get_db()
+        db = MongoClient().test_database
         error = None
 
         if not username:
             error = 'Username is required.'
         elif not password:
             error = 'Password is required.'
-        elif db.execute(
-            'SELECT id FROM user WHERE username = ?', (username,)
-        ).fetchone() is not None:
+        elif db.user.find_one({"username":username}) is not None:
             error = 'User {} is already registered.'.format(username)
 
         if error is None:
-            db.execute(
-                'INSERT INTO user (username, password) VALUES (?, ?)',
-                (username, generate_password_hash(password))
-            )
-            db.commit()
+            password = encrypt_passowrd(password)
+            db.user.insert_one({"username":username, "password":password})
             return redirect(url_for('auth.login'))
 
         flash(error)
 
     return render_template('auth/register.html')
+
+
+@bp.route('/login', methods=('GET', 'POST'))
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        db = MongoClient().test_database
+        error = None
+        user = db.user.find_one({"username":username})
+
+        if user is None:
+            error = 'Incorrect username.'
+        elif not verify_password(user.get("password"), password):
+            error = 'Incorrect password.'
+
+        if error is None:
+            session.clear()
+            session['user_id'] = user['_id']
+            return redirect(url_for('index'))
+
+        flash(error)
+
+    return render_template('auth/login.html')
+
+
+@bp.before_app_request
+def load_logged_in_user():
+    user_id = session.get('user_id')
+
+    if user_id is None:
+        g.user = None
+    else:
+        g.user = MongoClient().test_database.user.find_one({"_id": user_id})
